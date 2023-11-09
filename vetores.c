8876926b711e9630f3edbc7c8ae3ba5ef226726f
Tavaress00 <143638036+Tavaress00@users.noreply.github.com>
2023-11-09 14:38:22 -0300

    Add files via upload

---
+++ vetores.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int vetor[5];
+int i;
+
+printf("Digite os elementos do vetor: \n");
+for ( i = 0; i < 5; i++)
+{
+    printf("Elementos %d: ", i +1);
+    scanf("%d", &vetor[i]);
+}
+
+{
+    /* code */
+}
+
+    return 0;
+}
